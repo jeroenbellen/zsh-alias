e4084e9b4ca03a606fd0d93dbf716942ffff6f7b
Jeroen Bellen <bellenjeroen@gmail.com>
2019-03-04 08:52:45 +0100

    Create my.zsh

---
+++ my.zsh
@@ -0,0 +1,9 @@
+alias javaVersions="/usr/libexec/java_home -V"
+
+function switchJavaVersion() {
+    echo 'Switchting to version' $1
+    export JAVA_HOME=`/usr/libexec/java_home -v $1`
+}
+
+alias mci="mvn clean install -T 1C"
+alias yolo="mvn clean install -DskipTests -T 1C"
