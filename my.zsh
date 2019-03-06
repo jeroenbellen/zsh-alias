b117c5691d39fb3997916095de77e04505056852
Jeroen Bellen <bellenjeroen@gmail.com>
2019-03-06 09:56:13 +0100

    Update my.zsh

--- my.zsh
+++ my.zsh
@@ -7,3 +7,7 @@ function switchJavaVersion() {
 
 alias mci="mvn clean install -T 1C"
 alias yolo="mvn clean install -DskipTests -T 1C"
+
+function listeningOnPorts() {
+    netstat -Watnlv | grep LISTEN | awk '{"ps -o comm= -p " $9 | getline procname;colred="\033[01;31m";colclr="\033[0m"; print colred "proto: " colclr $1 colred " | addr.port: " colclr $4 colred " | pid: " colclr $9 colred " | name: " colclr procname;  }' | column -t -s "|"
+}
